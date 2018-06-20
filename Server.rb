d469197353334e1e328230d80b63ea211687d0a0
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2018-06-20 22:29:30 +0900

    [fix] メソッドに引数を渡すときは()を省略しないことにした

--- Server.rb
+++ Server.rb
@@ -14,13 +14,13 @@ class Server
   def start
     @status = 1
 
-    puts "mojat-server start... on:#{@port}"
-    @socket = TCPServer.open @port
+    puts("mojat-server start... on:#{@port}")
+    @socket = TCPServer.open(@port)
 
     channel = Channel.new
-    while @status == 1 do
+    while @status == 1
       connection = @socket.accept
-      channel.add_user connection
+      channel.add_user(connection)
     end
 
     @socket.close
@@ -38,33 +38,33 @@ class Channel
   end
 
   def add_user(user)
-    puts "#{user} connected!"
-    @users.push user
+    puts("#{user} connected!")
+    @users.push(user)
 
     Thread.start(user) do
       while (message = user.gets)
         message = "#{user.remote_address.ip_address}: #{message}"
-        puts message
-        cast message
+        puts(message)
+        cast(message)
       end
 
-      remove_user user
+      remove_user(user)
     end
   end
 
   def remove_user(user)
-    puts "#{user} disconnected!"
-    @users.delete user
+    puts("#{user} disconnected!")
+    @users.delete(user)
     user.close
   end
 
   def cast(message)
     @users.each do |user|
-      user.puts message
+      user.puts(message)
     end
   end
 end
 
 # main_task
-my_server = Server.new 20_000
+my_server = Server.new(20_000)
 my_server.start
