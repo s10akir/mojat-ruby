8055fee9cee13f36e7b286ed355f6aef13dc6182
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2018-06-20 22:19:37 +0900

    [new] Serverスクリプトの作成
    単純にテキストをやり取りするところまで

---
+++ Server.rb
@@ -0,0 +1,70 @@
+#! /usr/bin/env ruby
+# socket通信を用いてチャットを行うRubyスクリプト（サーバサイド）
+
+require 'socket'
+
+# サーバクラス
+class Server
+  @status = 0
+
+  def initialize(port)
+    @port = port
+  end
+
+  def start
+    @status = 1
+
+    puts "mojat-server start... on:#{@port}"
+    @socket = TCPServer.open @port
+
+    channel = Channel.new
+    while @status == 1 do
+      connection = @socket.accept
+      channel.add_user connection
+    end
+
+    @socket.close
+  end
+
+  def stop
+    @status = 0
+  end
+end
+
+# チャットを管理するクラス
+class Channel
+  def initialize
+    @users = []
+  end
+
+  def add_user(user)
+    puts "#{user} connected!"
+    @users.push user
+
+    Thread.start(user) do
+      while (message = user.gets)
+        message = "#{user.remote_address.ip_address}: #{message}"
+        puts message
+        cast message
+      end
+
+      remove_user user
+    end
+  end
+
+  def remove_user(user)
+    puts "#{user} disconnected!"
+    @users.delete user
+    user.close
+  end
+
+  def cast(message)
+    @users.each do |user|
+      user.puts message
+    end
+  end
+end
+
+# main_task
+my_server = Server.new 20_000
+my_server.start
